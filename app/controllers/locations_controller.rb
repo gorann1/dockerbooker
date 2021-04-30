4a8d0c41b7f59dd96b5ec91d6cdf41224d6eb772
goran <goran.novacic@gmail.com>
2021-04-30 09:23:57 +0200

    #CW_RanSack_Filter_start

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,8 @@ class LocationsController < ApplicationController
   layout @locations
     def index
       @locations = Location.all
+      @q = Location.ransack(params[:q])
+      @locations = @q.result(distinct: true)
     end
 
     def search
--- app/models/zone.rb
+++ app/models/zone.rb
@@ -2,5 +2,6 @@ class Zone < ApplicationRecord
   has_many :countries
   has_many :regions
   has_many :centers
+  has_many :locations
 
 end
