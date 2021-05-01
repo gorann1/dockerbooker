7cc596420de8a14c8e2f6dddb1a7289e053cf384
goran <goran.novacic@gmail.com>
2021-05-02 01:32:02 +0200

    #react_on_rails_setup_finished

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,4 +1,5 @@
-import ReactOnRails from "react-on-rails";
+import ReactOnRails from 'react-on-rails';
+
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
