42455413ad888bef70caa2396d447a5a261e5f75
goran <goran.novacic@gmail.com>
2021-05-06 11:02:55 +0200

    #centers_layout_locations_show#2

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -3,5 +3,8 @@ class CentersController < ApplicationController
   def index
     @centers = Center.all
     Center.order('id ASC').reorder('name DESC')
+    #@centers = Center.paginate(page: params[:page])
+    @centers = Center.paginate(page: params[:page], per_page: 2)
+
   end
 end
