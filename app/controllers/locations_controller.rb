35d7d273ce6f1a50bf6a0b1a96222afc250eba55
goran <goran.novacic@gmail.com>
2021-05-13 09:05:12 +0200

    #location_search_test

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,9 @@
 class LocationsController < ApplicationController
   layout @locations
     def index
+      Location.order('id ASC').reorder('name DESC')
       @q = Location.ransack(params[:q])
-      @locations = @q.result.includes(:category)
+      # @locations = @q.result.includes(:category)
       @locations = @q.result.paginate(page: params[:page], per_page: 9)
 
         @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
